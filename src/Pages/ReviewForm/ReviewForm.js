e7988100971b353409e0380f6c34ad8940cdeb9a
fazlarabby1 <fazlarabby41@gmail.com>
2022-11-09 14:34:10 +0600

    created the review form for the user

--- src/Pages/ReviewForm/ReviewForm.js
+++ src/Pages/ReviewForm/ReviewForm.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ReviewForm = () => {
+    const service = useLoaderData();
+    console.log(service);
+    const { _id, title, image, ratings, details, price } = service;
+
     return (
-        <div>
-            <h2>Provide Your Precious Review on {} Please!!!</h2>
+        <div className='mb-10'>
+            <h2 className='text-center font-semibold text-xl italic underline underline-offset-4 mb-7
+            '>Provide Your Precious Review on <span className='text-red-500'>{title}</span> Please!!!</h2>
+            <div>
+                <form>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                        <input name="fullName" type="text" placeholder="Full Name" className="input input-bordered w-full" required />
+                        <input name="image" type="text" placeholder="Image URL" className="input  input-bordered w-full" required />
+                        <input name="email" type="text" placeholder="Your Email" className="input input-bordered w-full" readOnly />
+                        <input name="ratings" type="text" placeholder="Provide Rating Value" className="input input-bordered w-2/3" required />
+                    </div>
+                    <textarea name="reviewDetails" className="textarea textarea-bordered h-32 w-1/3 mt-6" placeholder="Review details here" required></textarea>
+                    <br />
+                    <input className='btn btn-outline btn-primary' type="submit" value="Submit Review" />
+                </form>
+            </div>
         </div>
     );
 };
