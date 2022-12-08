58743e8e2b640576668b1869c988ed4afc036f79
fazlarabby1 <fazlarabby41@gmail.com>
2022-12-08 15:51:46 +0600

    changed color in reviews

--- src/Pages/MyReviews/ReviewCard/ReviewCard.js
+++ src/Pages/MyReviews/ReviewCard/ReviewCard.js
@@ -16,17 +16,17 @@ const ReviewCard = ({ review, handleReviewDelete, getReview }) => {
     
 
     return (
-        <div className="flex flex-col md:w-3/4 mx-auto p-6 space-y-6 overflow-hidden rounded-lg shadow-lg dark:text-gray-100 border my-5">
+        <div className="flex flex-col md:w-3/4 mx-auto p-6 space-y-6 overflow-hidden rounded-lg shadow-lg border my-5">
             <div className='flex justify-between'>
                 <div className="flex items-center space-x-4">
-                    <img alt="" src={photo} className="object-cover w-12 h-12 rounded-full shadow dark:bg-gray-500" />
+                    <img alt="" src={photo} className="object-cover w-12 h-12 rounded-full shadow" />
                     <p className="text-sm font-semibold">{name}</p>
                 </div>
                 <p className='flex items-center font-semibold'><FaStar className='text-warning mr-1' />{rating}</p>
             </div>
-            <p className="dark:text-gray-400 text-xl font-semibold"><span className='text-xl mr-2 underline underline-offset-4 text-info'>Service Name: </span> {reviewService.title}</p>
+            <p className="text-xl font-semibold"><span className='text-xl mr-2 underline underline-offset-4 text-info'>Service Name: </span> {reviewService.title}</p>
 
-            <p className="dark:text-gray-400"><span className='text-xl mr-2 underline underline-offset-4 text-info'>Your Comment: </span> {reviewDetails}</p>
+            <p ><span className='text-xl mr-2 underline underline-offset-4 text-info'>Your Comment: </span> {reviewDetails}</p>
 
             <hr />
             <div className='flex justify-between'>
